80999276d0a63feda94c3ffcb2fce66c0c9c8a29
Asura7969 <1402357969@qq.com>
2023-02-16 15:46:25 +0800

    [feat]-rust-client grpc

---
+++ grpc-client/rust-client/src/main.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+use proto::{
+    hello_service_client::{HelloServiceClient},
+    HelloReply, HelloRequest,
+};
+use tonic::{Request, Response as TonicResponse, Status};
+use tonic::transport::Endpoint;
+
+
+mod proto {
+    tonic::include_proto!("com.asura.grpc");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = Endpoint::from_static("http://127.0.0.1:9091");
+    let mut hello_cli = HelloServiceClient::connect(addr.clone()).await?;
+    let request = Request::new(HelloRequest {
+        name: "tonic".to_string(),
+        tags: HashMap::new()
+    });
+    let response = hello_cli.say_hello(request).await?;
+    println!("hello response: {:?}", response.into_inner());
+
+    let mut goodbye_cli = HelloServiceClient::connect(addr).await?;
+    let request = Request::new(HelloRequest {
+        name: "tonic".to_string(),
+        tags: HashMap::new()
+    });
+    let response = goodbye_cli.say_hello_out_stream(request).await?;
+    println!("goodbye response: {:?}", response.into_inner());
+
+    Ok(())
+}
